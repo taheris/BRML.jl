ec650be6296543daf8416c3b7f07265a4792fd24
Shaun Taheri <shaun@taheris.co.uk>
2013-08-08 22:38:58 +0100

    create test cases for general.jl

--- src/BRML.jl
+++ src/BRML.jl
@@ -8,7 +8,7 @@ include("general_matlab.jl")
 export
     # general.jl
     argmin, argmax, logsumexp, betaXGreaterBetaY, avgSigmaGauss, cap,
-    condexp
+    condexp, dirRand
 
     # general_matlab.jl
     bar3z, chi2test, condp, stateCount
--- test/test_general.jl
+++ test/test_general.jl
@@ -26,4 +26,30 @@ facts("Testing General Functions") do
         @fact argmin(matrix) => ([1 1 2], [12 2 26])
         @fact argmax(matrix) => ([2 2 1], [41 5 44])
     end
+    
+    context("logsumexp") do
+        @fact logsumexp([]) => :throws
+        @fact logsumexp(scalar) => 5
+        @fact logsumexp(array) => roughly(33.002)
+        @fact logsumexp(vector) => roughly(33)
+        @fact logsumexp(matrix) => roughly([41 5.04859 44])
+    end
+
+    context("avgSigmaGauss") do
+        @fact avgSigmaGauss(0, 0) => 0.5
+        @fact avgSigmaGauss(0.2, 0.3) => roughly(0.54718)
+    end
+
+    context("cap") do
+        @fact cap(-3, 2) => -2
+        @fact cap([-5:5], 1) => [-1,-1,-1,-1,-1,0,1,1,1,1,1]
+    end
+    
+    context("dirRand") do
+        x = dirRand([1:10], 5)
+        for i=1:5
+            @fact sum(x[:,i]) => roughly(1)
+        end
+    end
+    
 end
